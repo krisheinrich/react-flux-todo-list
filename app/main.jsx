801e97fa3c3879930950a9b3ebefab7feab67fc3
Kris Heinrich <krisheinrich@gmail.com>
2016-06-16 11:19:50 -0700

    dispatcher and view connection

--- app/main.jsx
+++ app/main.jsx
@@ -4,16 +4,17 @@ var ReactDOM = require('react-dom');
 console.log("Hey from JSX!");
 
 var GroceryItemList = require('./components/GroceryItemList.jsx');
+var groceryItemStore = require('./stores/GroceryItemStore.jsx');
+var initialItems = groceryItemStore.getItems();
 
-var initialItems = [{
-  name: "Ice Cream"
-},{
-  name: "Waffles"
-},{
-  name: "Candy",
-  purchased: true
-},{
-  name: "Snarks"
-}];
+function render() {
+  ReactDOM.render(<GroceryItemList items={initialItems}/>, app);
+}
 
-ReactDOM.render(<GroceryItemList items={initialItems}/>, app);
+groceryItemStore.onChange(function (items) {
+  initialItems = items;
+  render();
+});
+
+// Initial page load
+render();

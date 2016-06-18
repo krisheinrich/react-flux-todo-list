9dff8b46d70d45c9a864e5f82e7b72dfde5049d6
Kris Heinrich <krisheinrich@gmail.com>
2016-06-17 22:18:51 -0700

    added styling

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 var action = require('./../actions/GroceryItemActionCreator.jsx');
 
 module.exports = React.createClass({
+  
   getInitialState: function () {
     return {input: ""};
   },
+  
   addItem: function (e) {
     e.preventDefault();
     //console.log("Adding item", this.state.input);
@@ -16,17 +18,20 @@ module.exports = React.createClass({
       input: ''
     });
   },
+  
   handleInputName: function (e) {
     this.setState({input: e.target.value});
   },
+  
   render: function () {
     return (
       <div className="grocery-Additem">
         <form onSubmit={this.addItem}>
-          <input value={this.state.input} onChange={this.handleInputName}/>
+          <input value={this.state.input} type="text" onChange={this.handleInputName}/>
           <button>Add Item</button>
         </form>
       </div>
     );
   }
+
 });
--- server/main.js
+++ server/main.js
@@ -6,6 +6,7 @@ app.get('/', function (req, res) {
   res.render('./../app/index.ejs', {});
 })
 .use(express.static(__dirname + '/../.tmp'))
+.use(express.static(__dirname + '/../bower_components/skeleton/css'))
 .listen(7777);
 
 console.log("Listening on port 7777....");
